7735135ec491840f73af1a56ec97b01998222640
Loris199 <loris.marzullo@gmail.com>
2023-05-11 15:41:57 +0200

    Exemple utilisé pour la présentation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,15 @@
-import Troupes.*;
+import Army.*;
+import Army.Troups.Droid;
+import Army.Troups.Stormtrooper;
 
 public class Main {
-    public static void main(String[] args) throws CloneNotSupportedException {
+    public static void main(String[] args) {
+
         Stormtrooper t1 = new Stormtrooper();
-        System.out.println(t1);
-
         Stormtrooper t2 = (Stormtrooper) t1.copy();
-        System.out.println(t2);
-        System.out.println(t1);
-
         Stormtrooper t3 = (Stormtrooper) t1.copy();
-        System.out.println(t3);
-
         Droid d1 = new Droid();
-        System.out.println(d1);
         Droid d2 = (Droid) d1.copy();
-        System.out.println(d1);
-        System.out.println(d2);
-
-
-        Droid tmp = new Droid();
-        System.out.println(tmp);
-        Troup tmp2 = (Troup) tmp.copy();
-        System.out.println(tmp2);
-
 
         Squadron s = new Squadron();
         s.add(t1);
@@ -32,6 +18,9 @@ public class Main {
         s.add(d1);
         s.add(d2);
 
+        Squadron s2 = (Squadron) s.copy();
+
         System.out.println(s);
+        System.out.println(s2);
     }
 }
