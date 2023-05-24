75245fae353beffc066a8aa28cac0423379ccbfd
MasterZeus97 <thibault.seem@heig-vd.ch>
2023-05-24 15:10:47 +0200

    Added Army and utility method to Squadron

--- src/main/java/Army/Squadron.java
+++ src/main/java/Army/Squadron.java
@@ -2,36 +2,65 @@ package Army;
 
 import Army.Troups.Troup;
 
+import javax.naming.SizeLimitExceededException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Squadron implements Prototypeable {
-    static int count = 0;
-    int squadronNumber;
+
+    private final static int maxSizeSquadron = 10;
+
     private final List<Troup> troupList = new ArrayList<>();
 
     public Squadron() {
-        squadronNumber = ++count;
+
     }
 
-    public void add(Troup troup){
-        troupList.add(troup);
+    public void add(Troup troup) throws SizeLimitExceededException {
+        if(maxSizeSquadron > 10){
+            throw new SizeLimitExceededException("Squadron is already full");
+        }else{
+            troupList.add(troup);
+        }
+    }
+
+    public void clearSquadron(){
+        troupList.clear();
+    }
+
+    public int getMaxSize(){
+        return maxSizeSquadron;
+    }
+
+    public boolean isFull(){
+        if(troupList.size() < 10){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    List<Troup> getTroupList(){
+        return troupList;
     }
 
     @Override
     public Squadron copy() {
         Squadron tmp = new Squadron();
         for(Troup t : troupList){
-            tmp.add((Troup) t.copy());
+            try {
+                tmp.add((Troup) t.copy());
+            }catch (SizeLimitExceededException e){
+                System.out.println(e.getMessage());
+            }
         }
         return tmp;
     }
 
     @Override
     public String toString() {
-        StringBuilder returnString = new StringBuilder();
-        for(Troup t : troupList)
-            returnString.append(t).append("\n");
-        return "Squadron #" + squadronNumber + "\n" + returnString;
+        return "Squadron{" +
+                "troupList=" + troupList +
+                '}';
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import Army.*;
 import Army.Troups.Droid;
 import Army.Troups.Stormtrooper;
 
+import javax.naming.SizeLimitExceededException;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,11 +14,15 @@ public class Main {
         Droid d2 = (Droid) d1.copy();
 
         Squadron s = new Squadron();
-        s.add(t1);
-        s.add(t2);
-        s.add(t3);
-        s.add(d1);
-        s.add(d2);
+        try {
+            s.add(t1);
+            s.add(t2);
+            s.add(t3);
+            s.add(d1);
+            s.add(d2);
+        }catch (SizeLimitExceededException e){
+            System.out.println(e.getMessage());
+        }
 
         Squadron s2 = (Squadron) s.copy();
 
