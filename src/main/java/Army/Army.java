3fde78185ba1b09dac2eca404c8f4ec87d337f84
Loris199 <loris.marzullo@gmail.com>
2023-06-07 16:03:04 +0200

    Correction des erreurs de Thibault

--- src/main/java/Army/Army.java
+++ src/main/java/Army/Army.java
@@ -26,7 +26,7 @@ public class Army {
      * @return Le squadron à l'index squadronIndex
      */
     public Squadron getSquadron(int squadronIndex){
-        if(squadronIndex > 0 && squadronIndex < 5)
+        if(squadronIndex >= 0 && squadronIndex < 5)
             return squadronsList.get(squadronIndex);
         return null;
     }
