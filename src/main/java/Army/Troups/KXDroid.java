798901938b98f91bfaf6154f160417d9325cdc7f
Loris199 <loris.marzullo@gmail.com>
2023-06-11 22:47:55 +0200

    Nom plus court pour le KX droid

--- src/main/java/Army/Troups/KXDroid.java
+++ src/main/java/Army/Troups/KXDroid.java
@@ -1,6 +1,6 @@
 package Army.Troups;
 
-public class KXSeries extends Troup {
+public class KXDroid extends Troup {
 
    private static final int percentReduce = 10,
                             minHp = 100,
@@ -17,15 +17,15 @@ public class KXSeries extends Troup {
    /**
     * Constructeur d'un KXSeries
     */
-   public KXSeries() {
-      super("KX-series security droid", minHp, maxHp, minAtt, maxAtt, minDef, maxDef, minSpd, maxSpd, percentReduce);
+   public KXDroid() {
+      super("KX droid", minHp, maxHp, minAtt, maxAtt, minDef, maxDef, minSpd, maxSpd, percentReduce);
    }
 
    /**
     * Constructeur de copie d'un KXSeries
     * @param s KXSeries à copier
     */
-   public KXSeries(KXSeries s) {
+   public KXDroid(KXDroid s) {
       super(s);
    }
 
@@ -34,8 +34,8 @@ public class KXSeries extends Troup {
     * @return Un nouveau KXSeries, clone du premier
     */
    @Override
-   public KXSeries copy() {
-      KXSeries newTroup = new KXSeries(this);
+   public KXDroid copy() {
+      KXDroid newTroup = new KXDroid(this);
       newTroup.downGradeStat(downGradeStatChances);
       return newTroup;
    }
