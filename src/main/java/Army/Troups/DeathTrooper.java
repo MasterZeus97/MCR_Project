d3cee51e5a4e1ad9e54c32649684b13376cd326c
MasterZeus97 <thibault.seem@heig-vd.ch>
2023-06-11 23:07:46 +0200

    Added defeted prices to troups

--- src/main/java/Army/Troups/DeathTrooper.java
+++ src/main/java/Army/Troups/DeathTrooper.java
@@ -5,12 +5,14 @@ public class DeathTrooper extends Troup {
    private static final int percentReduce = 10,
                             minHp = 500,
                             maxHp = 600,
-                            minAtt = 15,
-                            maxAtt = 20,
+                            minAtt = 100,
+                            maxAtt = 110,
                             minDef = 200,
                             maxDef = 300,
                             minSpd = 5,
-                            maxSpd = 10;
+                            maxSpd = 10,
+                            minReward = 300,
+                            maxReward = 400;
 
    int downGradeStatChances = 70;
 
@@ -18,7 +20,7 @@ public class DeathTrooper extends Troup {
     * Constructeur d'un DeathTrooper
     */
    public DeathTrooper() {
-      super("Death Trooper", minHp, maxHp, minAtt, maxAtt, minDef, maxDef, minSpd, maxSpd, percentReduce);
+      super("Death Trooper", minHp, maxHp, minAtt, maxAtt, minDef, maxDef, minSpd, maxSpd, percentReduce, minReward, maxReward);
    }
 
    /**
