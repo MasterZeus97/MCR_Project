5530db2b087eb9d4a566fcf3b10ea29bbc580da1
joelmatias <joel.dsm@hotmail.com>
2023-06-11 17:06:31 +0200

    ajout d'activeAttacker

--- src/main/java/Combat/Combat.java
+++ src/main/java/Combat/Combat.java
@@ -12,7 +12,7 @@ public class Combat {
     Army army1, army2;
     Squadron squadronActive1, squadronActive2;
     int round;
-    Troup activeAttacked;
+    Troup activeAttacked, activeAttacker;
 
     List<Troup> timeline;
 
@@ -52,10 +52,10 @@ public class Combat {
             setTimeline();
         }
 
-        Troup attacker = timeline.remove(0);
+        activeAttacker = timeline.remove(0);
         Squadron squadronToAttack;
 
-        if(squadronActive1.getTroupList().contains(attacker))
+        if(squadronActive1.getTroupList().contains(activeAttacker))
             squadronToAttack = squadronActive2;
         else
             squadronToAttack = squadronActive1;
@@ -67,7 +67,7 @@ public class Combat {
         else
             activeAttacked = squadronToAttack.getTroupList().get(0);
 
-        activeAttacked.getAttacked(attacker.attack());
+        activeAttacked.getAttacked(activeAttacker.attack());
         if(activeAttacked.getStatsList().get(0).getValue() <= 0){
             squadronToAttack.getTroupList().remove(activeAttacked);
 
@@ -102,17 +102,17 @@ public class Combat {
     }
 
     public boolean isCombatFinished(){
-        boolean squadronsState1 = false,
-                squadronsState2 = false;
+        boolean squadronsState1 = true,
+                squadronsState2 = true;
 
         for(int i = 0; i < army2.getSquadronsList().size(); i++){
             if(army2.getSquadronsList().get(i).getTroupNumber() != 0)
-                squadronsState2 = true;
+                squadronsState2 = false;
         }
 
         for(int i = 0; i < army1.getSquadronsList().size(); i++){
             if(army1.getSquadronsList().get(i).getTroupNumber() != 0)
-                squadronsState1 = true;
+                squadronsState1 = false;
         }
 
         return squadronsState1 || squadronsState2;
@@ -123,7 +123,7 @@ public class Combat {
     }
 
     public Troup getTroupAttacker(){
-        return timeline.get(0);
+        return activeAttacker;
     }
 
 
